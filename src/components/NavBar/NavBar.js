e64815018d91f3412ef6459e9805a447a32623a7
Lucas Ordoñez <lordonez@technisys.com>
2021-11-24 15:57:01 -0300

    s

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,11 +2,13 @@ import "./NavBar.scss";
 
 const NavBar = () => {
   return (
-    <header>
-      <h1> Hola</h1>;
+    <header className="header">
       <nav>
         <ul>
-          <li>enlace 1</li>
+          <li className="li">enlace 1</li>
+          <li className="li">enlace 2</li>
+          <li className="li">enlace 3</li>
+          <li className="li">enlace 4</li>
         </ul>
       </nav>
     </header>
